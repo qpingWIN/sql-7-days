5613908821969402270fbbfb5c7bf47d99414678
Pavlo Petrashko <pavlo.petrashko@gmail.com>
2026-06-16 20:03:37 +0100

    Added a solution to Average Selling Price (LeetCode 1251)

--- day-3-joins/leetcode/average-selling-price.sql
+++ day-3-joins/leetcode/average-selling-price.sql
@@ -96,6 +96,13 @@ LEFT JOIN UnitsSold u
   ON p.product_id = u.product_id 
   AND u.purchase_date BETWEEN p.start_date AND p.end_date
 GROUP BY p.product_id;
+
+--OR THIS--
+
+SELECT p.product_id, CASE WHEN SUM(us.units) IS NULL THEN 0 ELSE ROUND(1.00*SUM(us.units*p.price)/SUM(us.units),2) END as average_price
+FROM Prices p
+LEFT JOIN UnitsSold us ON p.product_id = us.product_id AND us.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY p.product_id
 -- ============================================================
 -- THOUGHT PROCESS
 -- ============================================================
