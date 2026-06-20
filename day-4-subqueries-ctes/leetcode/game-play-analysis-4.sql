37f9df881f7e1b1e91d36ec11949293f3017d49b
Pavlo Petrashko <pavlo.petrashko@gmail.com>
2026-06-20 18:48:31 +0100

    Fixed last solution (LeetCode 550)

--- day-4-subqueries-ctes/leetcode/game-play-analysis-4.sql
+++ day-4-subqueries-ctes/leetcode/game-play-analysis-4.sql
@@ -87,13 +87,20 @@ WITH first_login AS (
     SELECT player_id, MIN(event_date) AS first_date
     FROM Activity
     GROUP BY player_id
+),
+joined AS (
+    SELECT
+        f.player_id,
+        MAX(CASE WHEN a.player_id IS NOT NULL THEN 1 ELSE 0 END) AS did_return --robust for possible duplicates per player
+    FROM first_login f
+    LEFT JOIN Activity a
+      ON f.player_id = a.player_id
+     AND a.event_date = DATE_ADD(f.first_date, INTERVAL 1 DAY)
+    GROUP BY f.player_id
 )
 SELECT
-    ROUND(AVG(CASE WHEN a.player_id IS NOT NULL THEN 1 ELSE 0 END), 2) AS fraction --clean trick to avoid calculating multiple row joins per player
-FROM first_login f
-LEFT JOIN Activity a
-  ON f.player_id = a.player_id
- AND a.event_date = DATE_ADD(f.first_date, INTERVAL 1 DAY);
+    ROUND(AVG(did_return), 2) AS fraction
+FROM joined;
 -- ============================================================
 -- THOUGHT PROCESS
 -- ============================================================
